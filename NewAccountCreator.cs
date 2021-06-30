f85dd1d4214dfa8cf0371149ae3cd49e4377ea55
Blessing Mashoko <bmashcom@hotmail.com>
2021-06-30 14:35:40 +0200

    Add project files.

---
+++ NewAccountCreator.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StandardChartered
+{
+    class NewAccountCreator
+    {
+
+        public Dictionary<string, string> accountDetails = new Dictionary<string, string>();
+
+        static void Main(string[] args)
+        {
+            var newAccount = new NewAccountCreator();
+           
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine("Welcome to Standard Chartered Bank");
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine("");
+
+            
+            
+            Console.WriteLine("1. PLEASE TELL US ABOUT YOURSELF?");
+
+            //get title and validate the input
+            EnterTitle:
+            Console.Write("Enter Title:");
+            String title = Console.ReadLine();
+            String titleError = Validator.titleValidation(title);
+            if (titleError != "")
+            {
+                Console.WriteLine(titleError);
+                goto EnterTitle;
+            }
+            
+            newAccount.accountDetails.Add("title",title);
+
+            //get firstname and validate the input
+            EnterFirstName:
+            Console.Write("Enter First Name:");
+            String firstName = Console.ReadLine();
+            String firstNameError = Validator.NameValidation(firstName);
+            if (firstNameError != "") {
+                Console.WriteLine(firstNameError);
+                goto EnterFirstName;
+            }
+            newAccount.accountDetails.Add("firstName", firstName);
+
+             //get firstname and validate the input
+             EnterMiddleName:
+            Console.Write("Enter Middle Name (Optional):");
+            String middleName = Console.ReadLine();
+            String middleNameError = Validator.NameValidation(middleName);
+            if (middleNameError != "")
+            {
+                Console.WriteLine(middleNameError);
+                goto EnterMiddleName;
+            }
+            newAccount.accountDetails.Add("middleName", middleName);
+
+            //get last and validate the input
+            EnterLastName:
+            Console.Write("Enter Last Name:");
+            String lastName = Console.ReadLine();
+            String lastNameError = Validator.NameValidation(lastName);
+            if (lastNameError != "")
+            {
+                Console.WriteLine(lastNameError);
+                goto EnterLastName;
+            }
+            newAccount.accountDetails.Add("lastName", lastName);
+
+
+            //get residence status and validate
+            EnterResidence:
+            Console.Write("Are you a resident of Zimbabwe? ");
+            String residence = Console.ReadLine();
+            String residenceError = Validator.NameValidation(residence);
+            if (residenceError != "")
+            {
+                Console.WriteLine(residenceError);
+                goto EnterResidence;
+            }
+            newAccount.accountDetails.Add("residence", residence);
+
+            //request passport number if resident of Zimbabwe
+            if (residence == "No")
+            {
+                EnterPassportNumber:
+                Console.Write("Enter passport number");
+                String passportNumber = Console.ReadLine();
+                if(passportNumber == "")
+                {
+                    Console.WriteLine("Please Enter Passport Number");
+                    goto EnterPassportNumber;
+                }
+                newAccount.accountDetails.Add("passportNumber", passportNumber);
+            }
+
+             //get dob and validate the input
+            EnterDateOfBirth:
+            Console.Write("Enter Date of Birth Name:");
+            String dateOfBirth = Console.ReadLine();
+            String dateOfBirthError = Validator.NameValidation(dateOfBirth);
+            if (dateOfBirthError != "")
+            {
+                Console.WriteLine(dateOfBirthError);
+                goto EnterDateOfBirth;
+            }
+            newAccount.accountDetails.Add("dateOfBirth", dateOfBirth);
+
+            Console.Write(newAccount.accountDetails.ToString());
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Validator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+
+namespace StandardChartered
+{
+    class Validator
+    {
+        public static String titleValidation(String title)
+        {
+            String[] validTitle ={ "Mr", "Mrs", "Ms", "Dr", "Prof", "Other" } ;
+            return (validTitle.Contains(title))?"":"Invalid input type any of the following options Mr, Mrs, Ms, Dr, Prof, Other";
+        }
+
+        public static String residenceValidation(String input)
+        {
+            String[] validInput = { "No", "Yes"};
+            return (validInput.Contains(input)) ? "" : "Invalid input type any of the following options 'Yes' or 'No'";
+        }
+
+        public static String NameValidation(String input,Boolean required=true)
+        {
+            return "";
+            //if (input.Length == 0 && required == false) return "";
+            //return (!Regex.Match(input, "^[A-Z][a-zA-Z]*$").Success) ? "" : "Please enter a valid name";
+        }
+
+        public static String DateOfBirthValidation(String input, Boolean required = true)
+        {
+            Match dobMatch = Regex.Match(input, @"^(?:(?:(?:0?[13578]|1[02])(\/|-|\.)31)\1|(?:(?:0?[1,3-9]|1[0-2])(\/|-|\.)(?:29|30)\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:0?2(\/|-|\.)29\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:(?:0?[1-9])|(?:1[0-2]))(\/|-|\.)(?:0?[1-9]|1\d|2[0-8])\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$");
+
+            return (!dobMatch.Success) ? "Date of Birth input format invalid" : "";
+        }
+      
+    }
+}
